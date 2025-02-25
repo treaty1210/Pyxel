b57ad7a045bf547237b6a2a0a5ae8b0d2f4adcc4
treaty1210 <135383414+treaty1210@users.noreply.github.com>
2025-02-25 11:01:37 -0800

    readjusted locations of text and buttons

--- my_game.py
+++ my_game.py
@@ -13,19 +13,19 @@ class App:
             pyxel.quit()
         
         if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
-            if 20 < pyxel.mouse_x < 40 and 65 < pyxel.mouse_y < 85:
+            if 30 < pyxel.mouse_x < 50 and 50 < pyxel.mouse_y < 70:
                 self.number -= 1
-            elif 100 < pyxel.mouse_x < 120 and 65 < pyxel.mouse_y < 85:
+            elif 100 < pyxel.mouse_x < 120 and 50 < pyxel.mouse_y < 70:
                 self.number += 1
-            elif 60 < pyxel.mouse_x < 80 and 90 < pyxel.mouse_y < 110:
+            elif 57 < pyxel.mouse_x < 77 and 80 < pyxel.mouse_y < 100:
                 pyxel.quit()
 
     def draw(self):
         pyxel.cls(pyxel.COLOR_LIME)
-        pyxel.text(70, 60, "Start", pyxel.COLOR_BLACK)
-        pyxel.text(70, 75, f'{self.number}', pyxel.COLOR_DARK_BLUE)
-        pyxel.text(30, 75, '-', pyxel.COLOR_DARK_BLUE)
-        pyxel.text(110, 75, '+', pyxel.COLOR_DARK_BLUE)
-        pyxel.text(70, 100, "Quit", pyxel.COLOR_BLACK)
+        pyxel.text(65, 30, "Start", pyxel.COLOR_BLACK)
+        pyxel.text(75, 60, f'{self.number}', pyxel.COLOR_DARK_BLUE)
+        pyxel.text(40, 60, '-', pyxel.COLOR_DARK_BLUE)
+        pyxel.text(110, 60, '+', pyxel.COLOR_DARK_BLUE)
+        pyxel.text(67, 90, "Quit", pyxel.COLOR_BLACK)
         
 App()
