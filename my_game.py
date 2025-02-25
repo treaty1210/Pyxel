99669b7f36e316e10fd0869d8fb3002c38d5f91d
treaty1210 <135383414+treaty1210@users.noreply.github.com>
2025-02-25 10:39:42 -0800

    added run to init

--- my_game.py
+++ my_game.py
@@ -4,6 +4,7 @@ import pyxel
 class App:
     def __init__(self):
         pyxel.init(160, 120, title="Sapoo Game")
+        pyxel.run(self.update, self.draw)
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_ESCAPE):
