1bedfa0252cb6527df459fa2dba912488925461c
treaty1210 <135383414+treaty1210@users.noreply.github.com>
2025-02-25 10:44:50 -0800

    added mouse functionality and number instance

--- my_game.py
+++ my_game.py
@@ -4,6 +4,8 @@ import pyxel
 class App:
     def __init__(self):
         pyxel.init(160, 120, title="Sapoo Game")
+        pyxel.mouse(True)
+        self.number = 0
         pyxel.run(self.update, self.draw)
 
     def update(self):
